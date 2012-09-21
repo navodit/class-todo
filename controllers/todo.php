ea8d1c8d6412dd00b25e4888538bc763022afb83
navodit <navodit.dhiman1991@gmail.com>
2012-09-21 15:58:02 +0530

    hello sir ,ihave done my task .plz see view_header

---
+++ controllers/todo.php
@@ -0,0 +1,12 @@
+<?php
+class todo extends CI_controller{
+
+function index(){
+
+   $this->load->view('header');
+
+}
+
+}
+
+?>
---
+++ views/header.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+
+<!--<link href="<?php echo base_url('/assets/css/bootstrap.min.css'); ?>" type="text/css" rel="stylesheet" />-->
+<link href="../../../assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<link href="../../../assets/css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css" />
+
+<script type="text/jscript" src="../../../assets/js/bootstrap.min.js" >
+</script>
+
+<title>todo</title>
+</head>
+
+<body>
+
+    <div class="row-fluid">
+    <div class="span12">    
+	<form>
+ 
+    <label>Label name</label>
+    <input class="input-xxlarge" type="text" placeholder="Some text ">
+
+    <span class="help-block">Example block-level help text here.</span>
+    <label class="checkbox">
+    <input type="checkbox"> Check me out
+    </label>
+    <button type="submit" class="btn">Submit</button>
+    </form>
+
+</div>
+ 
+    </div>
+ 
+			
+</body>
+</html>
